64e16d59632d0d7ba29f19122aef28ca0ab7398a
Alan Semenov <ase@enonic.com>
2016-01-06 15:30:26 +0100

    Wrapped js to give it scope and avoid naming conflicts

--- src/main/resources/site/assets/js/contentviewer.js
+++ src/main/resources/site/assets/js/contentviewer.js
@@ -1,30 +1,34 @@
-function getCurrentScript() {
-    var script = window.HTMLImports ? window.HTMLImports.currentScript : undefined;
+(function () {
 
-    if (!script && !!document.currentScript) {
-        script = document.currentScript.__importElement || document.currentScript;
+    function getCurrentScript() {
+        var script = window.HTMLImports ? window.HTMLImports.currentScript : undefined;
+
+        if (!script && !!document.currentScript) {
+            script = document.currentScript.__importElement || document.currentScript;
+        }
+
+        return script;
     }
 
-    return script;
-}
+    function getDocument(script) {
+        return script ? script.ownerDocument : document;
+    }
 
-function getDocument(script) {
-    return script ? script.ownerDocument : document;
-}
+    var dvScript = getCurrentScript();
+    var cvDocument = getDocument(dvScript);
+    var uid = cvDocument.baseURI.split('?uid=')[1];
 
-var dvScript = getCurrentScript();
-var cvDocument = getDocument(dvScript);
-var uid = cvDocument.baseURI.split('?uid=')[1];
+    function branchToggle(branch) {
+        var divs = getContainer('xpcontentviewerid').getElementsByTagName('div');
+        [].forEach.call(divs, function (el) {
+            el.style.display = el.className !== branch ? 'block' : 'none';
+        });
+        return false;
+    }
 
-function branchToggle(branch) {
-    var divs = getContainer('xpcontentviewerid').getElementsByTagName('div');
-    [].forEach.call(divs, function (el) {
-        el.style.display = el.className !== branch ? 'block' : 'none';
-    });
-    return false;
-}
+    function getContainer(containerId) {
+        containerId = containerId + '_' + uid;
+        return document.getElementById(containerId) || cvDocument.getElementById(containerId);
+    }
 
-function getContainer(containerId) {
-    containerId = containerId + '_' + uid;
-    return document.getElementById(containerId) || cvDocument.getElementById(containerId);
-}
+}());
