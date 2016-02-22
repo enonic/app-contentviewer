7745ce5faf3a4de1346993aeb30471eebb6bf8d1
Alan Semenov <ase@enonic.com>
2016-02-22 14:06:38 +0100

    Isolated click handler to avoid conflicts with other widgets

--- src/main/resources/site/assets/js/contentviewer.js
+++ src/main/resources/site/assets/js/contentviewer.js
@@ -1,33 +1,39 @@
-function cv_getCurrentScript() {
-    var script = window.HTMLImports ? window.HTMLImports.currentScript : undefined;
+(function (global) {
 
-    if (!script && !!document.currentScript) {
-        script = document.currentScript.__importElement || document.currentScript;
+    var cvDocument = cv_getDocument();
+    var uid = cvDocument.baseURI.split('?uid=')[1];
+
+    global.ContentViewer = {
+
+        cv_branchToggle: function (branch) {
+            var container = getContainer('xpcontentviewerid'),
+                otherBranch = (branch == "master" ? "draft" : "master");
+
+            if (container.querySelector("#" + branch).style.display == "none") {
+                container.querySelector("#" + branch).style.display = "block";
+                container.querySelector("#tab_" + branch).classList.toggle("selected");
+
+                container.querySelector("#" + otherBranch).style.display = "none";
+                container.querySelector("#tab_" + otherBranch).classList.toggle("selected");
+            }
+
+            return false;
+        }
+    };
+
+    function cv_getDocument() {
+        var script = window.HTMLImports ? window.HTMLImports.currentScript : undefined;
+
+        if (!script && !!document.currentScript) {
+            script = document.currentScript.__importElement || document.currentScript;
+        }
+
+        return script ? script.ownerDocument : document;
     }
 
-    return script;
-}
-
-function cv_getDocument(script) {
-    return script ? script.ownerDocument : document;
-}
-
-var dvScript = cv_getCurrentScript();
-var cvDocument = cv_getDocument(dvScript);
-var uid = cvDocument.baseURI.split('?uid=')[1];
-
-function cv_branchToggle(branch) {
-    var containerId = 'xpcontentviewerid_' + uid,
-        container = document.getElementById(containerId) || cvDocument.getElementById(containerId),
-        otherBranch = (branch == "master" ? "draft" : "master");
-
-    if (container.querySelector("#" + branch).style.display == "none") {
-        container.querySelector("#" + branch).style.display = "block";
-        container.querySelector("#tab_" + branch).classList.toggle("selected");
-
-        container.querySelector("#" + otherBranch).style.display = "none";
-        container.querySelector("#tab_" + otherBranch).classList.toggle("selected");
+    function getContainer(containerId) {
+        containerId = containerId + "_" + uid;
+        return document.getElementById(containerId) || cvDocument.getElementById(containerId);
     }
 
-    return false;
-}
+}(window));
