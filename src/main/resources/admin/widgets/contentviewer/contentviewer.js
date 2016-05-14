b491d10143f6fd531fb15a41c443949d15e33302
Alex Rodriguez <aro@enonic.com>
2016-05-14 10:10:23 +0200

    Colorized syntax highlighting. Updated XP dependency to latest release: 6.5.2

--- src/main/resources/admin/widgets/contentviewer/contentviewer.js
+++ src/main/resources/admin/widgets/contentviewer/contentviewer.js
@@ -25,8 +25,8 @@ function handleGet(req) {
     var params = {
         uid: uid,
         theme: 'atelier-cave-light',
-        contentDraft: draft ? JSON.stringify(draft, null, 2) : null,
-        contentMaster: master ? JSON.stringify(master, null, 2) : null,
+        contentDraft: draft ? highlightJson(draft) : null,
+        contentMaster: master ? highlightJson(master) : null,
         showMaster: activeBranch === 'master',
         showDraft: activeBranch === 'draft'
     };
@@ -37,3 +37,50 @@ function handleGet(req) {
     };
 }
 exports.get = handleGet;
+
+function highlightJson(json) {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, null, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+        function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = extraType(match) || 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="xpconview-' + cls + '">' + match + '</span>';
+        });
+}
+
+function extraType(value) {
+    if (value.length < 3) {
+        return null;
+    }
+    var v = value.substring(1, value.length - 1);
+    if (/^([^\/]:)?(\/[^\/]+)+$/g.test(v)) {
+        return 'path';
+    }
+    if (/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v)) {
+        return 'id'; // "2c17479b-c9be-406f-91e1-075f86bb9cde"
+    }
+    if (/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(v)) {
+        return 'datetime'; // "2016-05-13T10:28:23.910Z"
+    }
+    if (/([01]\d|2[0-3]):([0-5]\d)/.test(v)) {
+        return 'datetime'; // "00:01"
+    }
+    if (/^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])$/.test(v)) {
+        return 'datetime'; // "2016-05-13"
+    }
+
+    return null;
+}
