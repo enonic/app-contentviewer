b1f640291484a9ef6e7908dc18c2c1b0a288c2a3
Alan Semenov <ase@enonic.com>
2022-04-06 16:33:28 +0200

    Code review fixes (#169)

--- src/main/resources/admin/widgets/contentviewer/contentviewer.js
+++ src/main/resources/admin/widgets/contentviewer/contentviewer.js
@@ -60,7 +60,8 @@ function highlightJson(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
         function (match) {
-            let cls = 'number', title;
+            let cls = 'number';
+            let title;
             if (/^"/.test(match)) {
                 if (/:$/.test(match)) {
                     cls = 'key';
